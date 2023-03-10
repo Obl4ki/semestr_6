e50245c4e124e5f0460969198683140bda14257f
Obl4ki <lukaszoblak33@gmail.com>
2023-03-10 21:49:51 +0100

    java lab1

---
+++ javusia/cw1/src/test/java/edu/zut/pk/cw1/Cw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.zut.pk.cw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
